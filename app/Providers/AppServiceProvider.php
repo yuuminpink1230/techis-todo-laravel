26f01b0c0e937797ab68e43733ab1f18f0227618
yuuminpink1230 <yuuminpink@gmail.com>
2022-09-19 18:53:11 +0900

    change https develop

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,7 +26,7 @@ class AppServiceProvider extends ServiceProvider
     {
         Schema::defaultStringLength(191);
          //
-        if(\App::environment(['production'])){
+        if(\App::environment(['production']) || \App::environment(['develop'])){
            \URL::forceScheme('https');
         }
     }
